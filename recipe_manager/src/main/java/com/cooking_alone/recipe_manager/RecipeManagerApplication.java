ce249ede5759a35075cab4704e30942b5b5ed6d8
Brant-Harker <brant.harker12@gmail.com>
2026-01-09 16:20:29 -0700

    add missing files

---
+++ recipe_manager/src/main/java/com/cooking_alone/recipe_manager/RecipeManagerApplication.java
@@ -0,0 +1,15 @@
+package com.cooking_alone.recipe_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeManagerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RecipeManagerApplication.class, args);
+
+	}
+
+}
